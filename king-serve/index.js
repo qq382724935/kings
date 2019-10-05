07abd3d762d45c5b715c410fb0ec957a8edc4860
qq382724935 <382724935@qq.com>
2019-10-05 20:21:35 +0800

    修改index.js

--- king-serve/index.js
+++ king-serve/index.js
@@ -1,13 +1,14 @@
 /*
  * @Date: 2019-09-28 15:12:29
  * @LastEditors: 刘利军
- * @LastEditTime: 2019-10-03 16:08:55
+ * @LastEditTime: 2019-10-05 20:19:12
  */
 const express = require('express');
 const app = express();
 app.set('secret', 'i2u3ifu4843hhg8hg4849gh4g844fh38');
 app.use(require('cors')());
 app.use(express.json());
+app.use('/', express.static(__dirname + '/web'));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 require('./plugins/db')(app);
 require('./routes/admin')(app);
